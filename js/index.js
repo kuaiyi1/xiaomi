b9b63e6b142fc04242197a5d617e92a1f80ea1e3
kuaiyi1 <3192261624@qq.com>
2018-03-10 08:53:05 +0800

    i

--- js/index.js
+++ js/index.js
@@ -51,12 +51,16 @@
 		banner_rbtn.onclick=function(){
 			if(flag){
 				flag=false;
-			move();}}
+				move();
+				}
+		}
 		banner_lbtn.onclick=function(){
-		if(flag){
-		k-=2;
+			if(flag){
+				k-=2;
 				flag=false;
-		move();}}
+				move();
+				}
+		}
 	//transitionend
 	//addEventListener
      imgs.forEach(function(ele,index){
@@ -88,7 +92,7 @@
 		}
 		inner.style.marginLeft=-1240*n+"px";
 	}
-	 prev.onclick=function(){
+	prev.onclick=function(){
 		n--;
 		next.classList.remove("disable");
 		if(n===0){
@@ -100,7 +104,7 @@
 		}
 		inner.style.marginLeft=-1240*n+"px";
 	}
-}
+	}
 	const stars=document.querySelectorAll(".star");
 	stars.forEach(function(ele,index){
 			shan(ele);
@@ -132,19 +136,26 @@
 
 //转化开始
 {
+
 	function yi(every){
 	const left_btn=every.querySelector(".banner_lbtn1");
 	const right_btn=every.querySelector(".banner_rbtn1");
 	const  inner1=every.querySelector(".neirong_bottom_item1");
+	const  lefts=every.querySelectorAll(".neirong_bottom_left");
+	const  items=every.querySelectorAll(".neirong_bottom_item");
 	var j=0;
+	var obj=lefts[0];
 	right_btn.onclick=function(){
 			j++;
 			
-			if(j===3){
-				j=2;
+			if(j===items.length){
+				j=items.length-1;
 				return;
 			}
+			lefts[j].classList.add("neirong_bottom_left2");
+			lefts[j-1].classList.remove("neirong_bottom_left2");
 			inner1.style.marginLeft=-296*j+"px";
+			    obj=lefts[j];
 	}
 	left_btn.onclick=function(){
 		 j--;
@@ -153,11 +164,64 @@
 		 	 return;
 		 }
 		 inner1.style.marginLeft=-296*j+"px";
+		 lefts[j+1].classList.remove("neirong_bottom_left2");
+		 lefts[j].classList.add("neirong_bottom_left2");
+		 obj=lefts[j];
 	}
+
+
+	lefts.forEach(function(ele,index){
+		ele.onclick=function(){
+			// for(var i=0;i<lefts.length;i++){
+			// 	lefts[i].classList.remove("neirong_bottom_left2");
+			// }
+				obj.classList.remove("neirong_bottom_left2");
+				ele.classList.add("neirong_bottom_left2");
+				obj=ele;
+				inner1.style.marginLeft=-296*index+"px";
+				j=index;
+			}
+		}
+	)
 	}
 	const itemss=document.querySelectorAll(".neirong_bottom_item2");
 	itemss.forEach(function(ele,index){
 			yi(ele);
 	})
+	
 
+}
+//弹出操作
+{
+	let pops=document.querySelectorAll(".pop1");
+	let lis=document.querySelectorAll(".banner_nav li");
+	console.log(pops);
+	var obj=pops[0];
+	lis.forEach(function(ele,index){
+			ele.onmouseenter=function(){
+				obj.style.display="none";
+				pops[index].style.display="block";
+				obj=pops[index];
+			}
+			ele.onmouseleave=function(){
+				pops[index].style.display="none";
+			}
+	})
+}
+//gilde
+{
+	let  spanss=document.querySelectorAll(".nav_wenzi span");
+	let  gilders=document.querySelectorAll(".glide");
+	  
+	
+	spanss.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			gilders[index].style.height="100px";
+			gilders[index].style.display="block";
+		}
+		ele.onmouseleave=function(){
+			gilders[index].style.height="0";
+			gilders[index].style.display="none";
+		}
+	})
 }
